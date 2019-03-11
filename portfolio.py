0c1d2ff931513490f780ab6043decabb714c01ee
0x3F3F <26686669+0x3F3F@users.noreply.github.com>
2019-03-11 18:46:05 +0100

    new stock

--- portfolio.py
+++ portfolio.py
@@ -34,7 +34,7 @@ IDX_BUY_PRICE_ALERT  = 5	# Flag when it's reached my buy price
 NO_TARGET_PRICE	 =  -1
 NO_ALL_TIME_HIGH =  -1
 
-REFRESH_SECONDS = 600
+REFRESH_SECONDS = 300
 
 
 def	SetupShareDictionaries(currDict, shareDictMultiAss, shareDictGrowth, shareDictIndexes, pmDict):
@@ -57,24 +57,34 @@ def	SetupShareDictionaries(currDict, shareDictMultiAss, shareDictGrowth, shareDi
 	shareDictMultiAss['RICA.L'] = ['RICA',	'Ruffer',							'GBX',		1,		242,			NO_TARGET_PRICE]
 	shareDictMultiAss['BTEM.L'] = ['BTEM',	'British Empire',					'GBX',		0,		768,			600]
 	shareDictMultiAss['CLDN.L'] = ['CLDN',	'Caledonia',						'GBX',		0,		2907,			2500]
-	shareDictMultiAss['HANA.L'] = ['HANA',	'Hansa Trust',						'GBX',		0,		1030,			800]
 	shareDictMultiAss['HAST.L'] = ['HAST',	'Henderson Alternative',			'GBX',		0,		305,			250]
 
 	shareDictGrowth['SST.L'] =  ['SST',		'Scottish Oriental Smaller Cos',	'GBX',		0,		1098,			800] 
 	shareDictGrowth['AAS.L'] =  ['AAS',		'Aberdeen Asian Smaller Cos',		'GBX',		0,		1143,			800] 
 	shareDictGrowth['BRFI.L'] = ['BRFI',	'Blackrock Frontiers',				'GBX',		0,		169,			130] 
 	shareDictGrowth['AFMC.L'] = ['AFMC',	'Aberdeen Frontiers',				'GBX',		1,		68.2,			50]
-	shareDictGrowth['ANII.L'] = ['ANII',	'Aberdeen New India',				'GBX',		0,		487,			400]
 	shareDictGrowth['JII.L'] =  ['JII',		'JP Morgan Indian',					'GBX',		0,		786,			600]
-	shareDictGrowth['AJOT.L'] = ['AJOT',	'AVI Japan Opportunities',			'GBX',		0,		102,			80]
 	shareDictGrowth['HRI.L'] =  ['HRI',		'Herald',							'GBX',		0,		1380,			800] 
 	shareDictGrowth['BIOG.L'] = ['BIOG',	'Biotech Growth Trust',				'GBX',		0,		836,			500]
 	shareDictGrowth['PIN.L'] =  ['PIN',		'Pantheon',							'GBX',		0,		2179,			1200] 
 	shareDictGrowth['HGT.L'] =  ['HGT',		'HG Capital',						'GBX',		0,		2030,			1200] 
 	shareDictGrowth['IBTS.L'] = ['IBTS',	'iShares US Treas 1-3',				'GBX',		2,		109,			85]
-	
-	currDict['GBPEUR=X']	 =	['GBPEUR',	'GBP to EUR XRate',					'',			3,		NO_ALL_TIME_HIGH, NO_TARGET_PRICE]
-	currDict['GBPUSD=X']	=	['GBPUSD',	'GBP to USD XRate',					'',			3,		NO_ALL_TIME_HIGH, NO_TARGET_PRICE]
+	shareDictGrowth['IBTM.L'] = ['IBTM',	'iShares US Treas 7-10',			'GBX',		2,		169,			140]
+	shareDictGrowth['IBTL.L'] = ['IBTL',	'iShares US Treas 20+',				'GBX',		2,		412,			330]
+	shareDictGrowth['TP05.L'] = ['TP05',	'iShares US TIPs 0-5',				'GBX',		2,		389,			350]
+	shareDictGrowth['YCA.L'] =  ['YCA',		'Yellowcake',						'GBX',		0,		250,			210] 
+	shareDictGrowth['GCL.L'] =  ['GCL',		'Geiger Counter',					'GBX',		1,		NO_ALL_TIME_HIGH,NO_TARGET_PRICE] 
+	shareDictGrowth['NXE.TO'] =  ['NXE',	'Nexgen Energy',					'CAD',		2,		3.8,			2.39] 
+	shareDictGrowth['URE.TO'] =  ['URE',	'UR-Energy',						'CAD',		2,		2.00,			0.88] 
+	shareDictGrowth['DML.TO'] =  ['DML',	'Denison Mines',					'CAD',		2,		3.75,			0.6] 
+	shareDictGrowth['FCU.TO'] =  ['FCU',	'Fission Uranium Corp',				'CAD',		2,		1.6,			0.54] 
+	shareDictGrowth['LAM.TO'] =  ['LAM',	'Laramide Resources',				'CAD',		3,		2.6,			0.34] 
+	shareDictGrowth['UEX.TO'] =  ['UEX',	'UEX Corporation',					'CAD',		3,		2.2,			0.17] 
+	shareDictGrowth['GXU.V'] =  ['GXU',		'Goviex',							'CAD',		3,		0.38,			0.10] 
+
+	currDict['GBPEUR=X']	 =	['GBPEUR',	'GBP to EUR XRate',					'',			4,		NO_ALL_TIME_HIGH, NO_TARGET_PRICE]
+	currDict['GBPUSD=X']	=	['GBPUSD',	'GBP to USD XRate',					'',			4,		NO_ALL_TIME_HIGH, NO_TARGET_PRICE]
+	currDict['GBPCAD=X']	=	['GBPCAD',	'GBP to CAD XRate',					'',			4,		NO_ALL_TIME_HIGH, NO_TARGET_PRICE]
 
 def GetTabs(Ticker):
 	"""Get the number of tabs depending on length of ticker so stuff lines up"""
